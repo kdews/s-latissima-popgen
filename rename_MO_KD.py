22a99cdfa27ac3af8861000e21d9320e83d35207
kdews <kdeweese@usc.edu>
2025-12-04 23:23:09 -0800

    Auto-commit: Thu Dec  4 23:23:01 PST 2025

--- rename_MO_KD.py
+++ rename_MO_KD.py
@@ -7,7 +7,7 @@ import subprocess
 # into a specified new directory.
 
 ## Files we want to rename are in the format:
-## UniqueID_AmplificationType_Genus_species_SampleID_Plate_Barcode_Genus_Sequencer_Lane_Read.fastq.gz
+## Library_AmplificationType_Genus_species_SampleID_Plate_Barcode_Genus_RunID_Lane_Read.fastq.gz
 ## e.g.
 ## IZYN_NanoAmplified_Saccharina_angustissima_SA-CB-5-MG-3_1_GCAATGCA_Saccharina_I997_L1_R1.fastq.gz
 ## IZYN_NanoAmplified_Saccharina_angustissima_SA-CB-5-MG-3_1_GCAATGCA_Saccharina_I997_L1_R2.fastq.gz
@@ -18,7 +18,9 @@ import subprocess
 ## JCGT_NanoAmplified_Saccharina__LIS-F1-3_3_TCTGTTGG_Saccharina_I1018_L1_R1.fastq.gz
 
 ## Renamed format:
-## UniqueID_SampleID_Sequencer_Barcode_Plate_Lane_Read.fastq.gz
+## SampleID_Library_RunID_Barcode_Plate_Lane_Read.fastq.gz
+## e.g.
+## LIS-F1-3_JCGT_I1018_TCTGTTGG_3_L1_R1.fastq.gz
 
 # Specify PATH to directory containing input files
 og_file = sys.argv[1]
@@ -33,30 +35,30 @@ with open(og_file, 'r') as files:
 			file_list = file_stripped.rsplit('_')
 			alts = ['I1018','I1019']
 			if 'LIS' in file_stripped:
-				uniqueid = file_list[0]
 				sampleid = file_list[4]
-				sequencer = file_list[8]
+				library = file_list[0]
+				runid = file_list[8]
 				plate = file_list[5]
 				barcode = file_list[6]
 				lane = file_list[9]
 				read_fq = file_list[10]
 			elif any(item in file_stripped for item in alts):
-				uniqueid = file_list[0]
 				sampleid = file_list[5]
-				sequencer = file_list[9]
+				library = file_list[0]
+				runid = file_list[9]
 				plate = file_list[6]
 				barcode = file_list[7]
 				lane = file_list[10]
 				read_fq = file_list[11]
 			else:
-				uniqueid = file_list[0]
 				sampleid = file_list[4]
-				sequencer = file_list[8]
+				library = file_list[0]
+				runid = file_list[8]
 				plate = file_list[5]
 				barcode = file_list[6]
 				lane = file_list[9]
 				read_fq = file_list[10]
-			newname = uniqueid + '_' + sampleid + '_' + sequencer + '_' + barcode + '_' + plate + '_' + lane + '_' + read_fq
+			newname = sampleid + '_' + library + '_' + runid + '_' + barcode + '_' + plate + '_' + lane + '_' + read_fq
 			# Write list of rsync commands (-c verifies file content)
 			cmd = f'rsync -ac --progress {file.strip()} {os.path.join(outdir, newname)}\n'
 			cmd_list.write(cmd)
